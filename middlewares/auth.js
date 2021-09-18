dc5eb651c40591f3db1dba61a1f2895f80c53e0e
thanhlam1998 <lamnguyen11e@gmail.com>
2021-09-19 02:24:00 +0700

    add API current user to get current logged in user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,6 @@ exports.authCheck = async (req, res, next) => {
     const firebaseUser = await admin
       .auth()
       .verifyIdToken(req.headers.authtoken);
-    console.log("FIREBASE USER IN AUTHCHECK", firebaseUser);
     req.user = firebaseUser;
     next();
   } catch (error) {
