f170fc046c2dfb7a396583b2528f7b46b74c57b0
thanhlam1998 <lamnguyen11e@gmail.com>
2021-09-21 13:28:51 +0700

    Show products by count and remove product

--- controllers/product.js
+++ controllers/product.js
@@ -12,7 +12,23 @@ exports.create = async (req, res) => {
   }
 };
 
-exports.read = async (req, res) => {
-  const products = await Product.find({});
+exports.listAll = async (req, res) => {
+  const products = await Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate("category")
+    .populate("subs")
+    .sort([["createdAt", "desc"]])
+    .exec();
   res.json(products);
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Product.findOneAndRemove({
+      slug: req.params.slug,
+    }).exec();
+    res.json(deleted);
+  } catch (error) {
+    return res.status(400).send("Product delete failed");
+  }
 };
