56732a0485e7e54a46cf8a95602d2be91283f3c5
thanhlam1998 <lamnguyen11e@gmail.com>
2021-09-25 01:08:00 +0700

    add route post save address

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,18 @@ const router = express.Router();
 const { authCheck } = require("../middlewares/auth");
 
 // controller
-const { userCart, getUserCart, emptyCart } = require("../controllers/user");
+const {
+  userCart,
+  getUserCart,
+  emptyCart,
+  saveAddress,
+} = require("../controllers/user");
 
 // routes
 router.get("/user/cart", authCheck, getUserCart);
 router.post("/user/cart", authCheck, userCart);
 router.put("/user/cart", authCheck, emptyCart);
+router.post("/user/address", authCheck, saveAddress);
 
 // router.get("/user", (req, res) => {
 //   res.json({
